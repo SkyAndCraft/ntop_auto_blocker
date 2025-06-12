c75b184462144f94f57cc117972514dfb278dce3
SkyAndCraft <fusella.michel@outlook.fr>
2025-06-12 17:11:34 +0200

    Update init_firewall_project.sh

--- init_firewall_project.sh
+++ init_firewall_project.sh
@@ -204,7 +204,7 @@ After=network.target docker.service
 
 [Service]
 Type=oneshot
-WorkingDirectory=/opt/ntop_auto_blocker
+WorkingDirectory=/opt/skyfirewall
 ExecStart=/usr/bin/docker compose up -d
 ExecStop=/usr/bin/docker compose down
 RemainAfterExit=true
